c842659d927b49a0b2264e1f3eff08607a46ed74
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2024-08-31 16:13:23 -0400

    added numbers to brailleMap

--- typescript/data/braille-translator.ts
+++ typescript/data/braille-translator.ts
@@ -28,6 +28,16 @@ export const brailleMap: { [key: string]: any } = {
   cap: ".....O",
   num: ".O.OOO",
   " ": "......",
+  0: ".OOO..",
+  1: "O.....",
+  2: "O.O...",
+  3: "OO....",
+  4: "OO.O..",
+  5: "O..O..",
+  6: "OOO...",
+  7: "OOOO..",
+  8: "O.OO..",
+  9: ".OO...",
 };
 
 export const brailleNumbers: { [key: string]: string } = {
