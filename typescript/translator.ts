9d773519e4b26f5b955002712d12cb5652e93388
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2024-08-31 15:01:27 -0400

    defined args

--- typescript/translator.ts
+++ typescript/translator.ts
@@ -1,7 +1,10 @@
 import { translateToEnglish, translateToBraille } from "./functions/functions";
+
 const args = process.argv.slice(2);
 
 if (args.length < 2) {
   console.error("Usage: ts-node cli.ts <translateTo|translateFrom> <text>");
   process.exit(1);
 }
+
+const [command, text] = args;
