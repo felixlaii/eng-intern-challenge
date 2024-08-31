45a9aac0d40f786f7b8079f41d942cffc109d3ad
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2024-08-31 14:19:57 -0400

    create interface and function for translatetoBraille

---
+++ typescript/functions/functions.ts
@@ -0,0 +1,13 @@
+import {
+  brailleMap,
+  brailleNumbers,
+  englishMap,
+} from "../data/braille-translator";
+
+export interface translateToBrailleProps {
+  text: string;
+}
+
+export function translateToBraille(props: translateToBrailleProps): any {
+  let brailleText = "";
+}
